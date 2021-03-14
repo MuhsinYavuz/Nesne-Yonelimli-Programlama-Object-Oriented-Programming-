660da48f851104e9712cb675d394fc662bc272a9
Muhsin <80474514+MuhsinYavuz@users.noreply.github.com>
2021-03-15 00:29:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IKrediManager ihtıyacKrediManager = new IhtıyacKrediManager();
+            IKrediManager taşıtKrediManager = new TaşıtKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager esnafKrediManager = new EsnafKredisiManager();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+        //    basvuruManager.BasvuruYap(ihtıyacKrediManager);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtıyacKrediManager,taşıtKrediManager };
+        //    basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+            ILogerService FileLogger = new FileLoggerService();
+            ILogerService DataLogger = new DatabaseLoggerService();
+            ILogerService SmsLogeer = new SmsLoggerService();
+            basvuruManager.BasvuruYap(esnafKrediManager,new List<ILogerService> {DataLogger,SmsLogeer,FileLogger });
+
+        }
+    }
+}
---
+++ SmsLoggerService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class SmsLoggerService : ILogerService
+
+    {
+        public void Log()
+        {
+            Console.WriteLine("SMS GÖNDERİLDİ.");
+        }
+    }
+}
